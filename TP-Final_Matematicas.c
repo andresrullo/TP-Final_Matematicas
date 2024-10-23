52b3f4fb85309611e7197e5f07b36a36c4c12c2b
Andrés Rullo <rulloandres@gmail.com>
2024-10-23 15:48:01 -0300

    Pruebas

--- TP-Final_Matematicas.c
+++ TP-Final_Matematicas.c
@@ -4,28 +4,87 @@
 #include <string.h>
 #include <ctype.h>
 
-int main() {
+void main() {
     char variable1;
     char variable2;
+    char resultado;
+    int option;
 
     printf("Ingrese la primer variable: \n");
-    scanf("%c", &variable1);
-
-    while (variable1 != 'v' && variable1 != 'V')
+    scanf("%c", &variable1);  /*CONVERTIR A MAYUSCULA LA LETRA INGRESADA*/
+    /*while (variable1 != 'V')
     {
         printf("La variable es incorrecta. Ingresela nuevamente\n");
         scanf("%c", &variable1);
-    };
-    
-    /*do
+    };*/
+
+    printf("Ingrese la segunda variable: \n");
+    scanf("%c", &variable2);  /*CONVERTIR A MAYUSCULA LA LETRA INGRESADA*/
+    /*while (variable2 != 'V')
     {
-        
-        if (variable1 != 'v' && variable1 != 'V')
-        {
-            printf("La variable ingresada no es correcta");
-        }
-    } while (variable1 != 'v' && variable1 != 'V');*/
+        printf("La variable es incorrecta. Ingresela nuevamente\n");
+        scanf("%c", &variable2);
+    };*/
     
+    printf("Seleccione una opción según la operacion deseada \n");
+    printf("1) Suma \n");
+    printf("2) \n");
+    printf("3) \n");
+    printf("4) \n");
+    printf("5) \n");
+    printf("6) \n");
+    
+
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            switch (variable1)  /*CHEQUEAR LA TABLA PARA VER SI ESTÁN BIEN LOS RESULTADOS*/
+            {
+            case 'V':
+                if (variable2 == 'V')
+                {
+                    resultado = "V";
+                }
+                    else if (variable2 = "F")
+                    {
+                        resultado = "F";
+                    }
+                break;
+            case 'F':
+                if (variable2 == 'F')
+                {
+                    resultado = "F";
+                }
+                else if (variable2 = "V")
+                {
+                    resultado = "V";
+                }
+                break;
+            }
+            break;
+        
+        case 2:
+            break;
+            
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        case 5:
+            break;
+
+        case 6:
+            break;
+
+        default:
+            printf("La opción ingresada no existe \n");
+            printf("Ingrese una opción correcta \n");
+            break;
+        }
+
 
     return 0;
 }
